c65687b7b65bedb10552f5b3b86dfa52d103edc5
Cyril Barillet <cyril.barillet@gmail.com>
2017-06-05 07:16:40 +0200

    Add node version in log.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var nodeVersion = JSON.stringify(process.version);
 
 app.get('/', function (req, res) {
-  res.send('MNAS is up with node version ' + JSON.stringify(process.version, null, 4));
+  res.send('MNAS is up with node version ' + nodeVersion);
 });
 
 app.listen(3333, function () {
-  console.log('MNA listening on port 3333!');
+  console.log('MNA listening on port 3333 ! Node version : ' + nodeVersion);
 });
 
