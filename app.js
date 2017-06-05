4d147ab61f042b99970876666e50bec78a02020c
Cyril Barillet <cyril.barillet@gmail.com>
2017-06-05 07:21:15 +0200

    Replace node version by all information.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-var nodeVersion = JSON.stringify(process.version);
+var nodeVersion = JSON.stringify(process.versions, null, 4);
 
 app.get('/', function (req, res) {
-  res.send('MNAS is up with node version ' + nodeVersion);
+  res.send('MNAS is up with ' + nodeVersion);
 });
 
 app.listen(3333, function () {
-  console.log('MNA listening on port 3333 ! Node version : ' + nodeVersion);
+  console.log('MNA listening on port 3333 !');
+  console.log(nodeVersion);
 });
 
